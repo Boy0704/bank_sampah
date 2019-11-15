d8615524c02d358da2679944d8e94d5678d1a415
Boy Kurniawan <boykurniawan123@gmail.com>
2019-11-15 07:18:34 +0700

    tabel penarikan di login nasabah

--- application/views/tabungan_nasabah.php
+++ application/views/tabungan_nasabah.php
@@ -2,6 +2,7 @@
         <div class="alert alert-info">
             <h3>Total Saldo Tabungan : Rp. <?php echo number_format(total_tabungan($this->session->userdata('id_user'))); ?></h3>
         </div>
+        <h4>Data Tabungan</h4>
         <table class="table table-bordered" style="margin-bottom: 10px" id="">
         	<thead>
             <tr>
@@ -15,6 +16,8 @@
             </tr>
             </thead>
             <tbody><?php
+            $total_penarikan = 0;
+            $id = $this->session->userdata('id_user');
             $total = 0;
             $start = 0;
             $pembelian_data = $this->db->get_where('pembelian', array('tabungan' => 'ya','id_anggota'=>$this->session->userdata('id_user')))->result();
@@ -43,14 +46,42 @@
         </table>
 
         <?php
-        $id_user = $this->uri->segment(3);
-        $cektabungan = $this->db->get_where('tabungan', array('id_user' => $id_user));
-		if ($cektabungan->num_rows() == 0) {
-			$this->db->insert('tabungan', array('id_user'=> $id_user, 'tabungan'=>$total));
-		} else {
-			$this->db->where('id_user',$id_user);
-			$this->db->update('tabungan',array('tabungan'=>$total));
-		}
+  //       $id_user = $this->uri->segment(3);
+  //       $cektabungan = $this->db->get_where('tabungan', array('id_user' => $id_user));
+		// if ($cektabungan->num_rows() == 0) {
+		// 	$this->db->insert('tabungan', array('id_user'=> $id_user, 'tabungan'=>$total));
+		// } else {
+		// 	$this->db->where('id_user',$id_user);
+		// 	$this->db->update('tabungan',array('tabungan'=>$total));
+		// }
 
         ?>
+
+        <br><br>
+        <h4>Data Penarikan Tabungan</h4>
+        <div class="row" style="margin-left: 10px;">
+    <table class="table table-bordered">
+        <tr>
+            <th>No.</th>
+            <th>Tanggal</th>
+            <th>No Nasabah</th>
+            <th>Jumlah Penarikan</th>
+        </tr>
+
+        <?php 
+        $no = 1;
+        foreach ($this->db->get_where('penarikan',array('id_anggota'=>$id))->result() as $rw) {
+         ?>
+        <tr>
+            <td><?php echo $no; ?></td>
+            <td><?php echo $rw->tanggal; ?></td>
+            <td><?php echo 'AGT00'.$rw->id_anggota.'-'.ambil_field_tabel('anggota','id_anggota',$rw->id_anggota,'nama_anggota') ?></td>
+            <td><?php echo number_format($rw->jumlah); $total_penarikan=$total_penarikan+$rw->jumlah; ?></td>
+        </tr>
+        <?php $no++; } ?>
+        <tr>
+                <td colspan="3">Total</td>
+                <td ><b><?php echo number_format($total_penarikan) ?></b></td>
+            </tr>
+    </table>
         
